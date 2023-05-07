345c0576dcfca1f2f70e6d77623d7e0fa3955535
AAMILITAO <t.i.militao@gmail.com>
2023-05-07 12:05:07 -0300

    Estrutura kafka producer

---
+++ api-service-producer/src/test/java/com/github/aamilitao/apiserviceproducer/ApiServiceProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aamilitao.apiserviceproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
