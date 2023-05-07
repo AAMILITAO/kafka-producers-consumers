3f73cee1369c63533b03aba437ef0212c2c590f9
AAMILITAO <t.i.militao@gmail.com>
2023-05-07 20:46:23 -0300

    Implementaçao kafka consumer a e b

---
+++ api-consumer-b/src/main/java/com/github/aamilitao/apiconsumerb/listener/MessageConsumer_B.java
@@ -0,0 +1,31 @@
+package com.github.aamilitao.apiconsumerb.listener;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import lombok.RequiredArgsConstructor;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class MessageConsumer_B {
+	
+	
+    @Value("${topic.name.consumer")
+    private String topicName;
+    
+    @KafkaListener(topics = "${topic.name.consumer}", groupId = "group_id")
+    public void consume(ConsumerRecord<String, String> payload){
+    	log.info("Consumer B - porta 8082");
+        log.info("Tópico: {}", topicName);
+        log.info("key: {}", payload.key());
+        log.info("Headers: {}", payload.headers());
+        log.info("Partion: {}", payload.partition());
+        log.info("Order: {}", payload.value());
+
+    }
+
+}
