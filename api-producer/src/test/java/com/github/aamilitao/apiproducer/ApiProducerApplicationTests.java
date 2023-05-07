6fde8b7aa8828e6ef255b8c3d15ceb255655b2e2
AAMILITAO <t.i.militao@gmail.com>
2023-05-07 12:57:23 -0300

    Estrutura do projeto

---
+++ api-producer/src/test/java/com/github/aamilitao/apiproducer/ApiProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aamilitao.apiproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
